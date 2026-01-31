2e94de5ec7c739b00aa1549e24c947844c757786
ouzman376@gmail.com <ouzman376@gmail.com>
2026-01-31 17:11:06 +0100

    Ton message (ex: ui fix / layer / map editor)

--- mybackend/wsgi.py
+++ mybackend/wsgi.py
@@ -1,19 +1,13 @@
-"""
-WSGI config for mybackend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-settings_module = "mybackend.deployment_settngs" if "RENDER_EXTERNAL_HOSNAME" in os.environ else "mybackend.settings"
+settings_module = (
+    "mybackend.deployment_settings"
+    if "RENDER_EXTERNAL_HOSTNAME" in os.environ
+    else "mybackend.settings"
+)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
-
-application = get_wsgi_application()
+app = get_wsgi_application()      # <- IMPORTANT pour Vercel
+application = app                 # <- alias Django classique
