282197dbcfab8394b1ccfff3b89da5d359d37b9b
ouzman376@gmail.com <ouzman376@gmail.com>
2026-02-01 01:49:11 +0100

    Dockerize backend for GeoDjango (GDAL/GEOS/PROJ)

--- mybackend/asgi.py
+++ mybackend/asgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
 
-# Le plus fiable : laisser Render définir DJANGO_SETTINGS_MODULE
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     os.getenv("DJANGO_SETTINGS_MODULE", "mybackend.settings")
