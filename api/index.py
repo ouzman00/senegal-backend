8c2a27b495d87e93948e5e314e8bf75947a80d38
ouzman376@gmail.com <ouzman376@gmail.com>
2026-02-03 09:57:46 +0100

    Premier commit

--- api/index.py
+++ api/index.py
@@ -2,4 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mybackend.settings")
-app = get_wsgi_application()
+application = get_wsgi_application()
