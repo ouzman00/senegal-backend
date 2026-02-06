a73cd10e58443ddb836b3ac2071e046b7be25d4f
ouzman376@gmail.com <ouzman376@gmail.com>
2026-02-06 21:49:12 +0100

    maj couche

--- maps/models.py
+++ maps/models.py
@@ -16,3 +16,11 @@ class Ecole(models.Model):
 
     def __str__(self):
         return self.nom
+
+class parcelles(models.Model):
+    nom = models.CharField(max_length=200)
+    adresse = models.CharField(max_length=255, blank=True, null=True)
+    geom = models.MultiPolygonField(srid=4326, blank=True, null=True)
+
+    def __str__(self):
+        return self.nom
