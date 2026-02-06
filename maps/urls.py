f85c2072ba745e915359b8aede4584461eaccdcc
ouzman376@gmail.com <ouzman376@gmail.com>
2026-02-06 23:20:38 +0100

    add parcelles endpoint

--- maps/urls.py
+++ maps/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import HopitalViewSet, EcoleViewSet
+from .views import HopitalViewSet, EcoleViewSet, ParcellesViewSet
 
 router = DefaultRouter()
 router.register(r"hopitaux", HopitalViewSet, basename="hopitaux")
 router.register(r"ecoles", EcoleViewSet, basename="ecoles")
+router.register(r"parcelles", ParcellesViewSet, basename="parcelles")
 
 urlpatterns = router.urls
