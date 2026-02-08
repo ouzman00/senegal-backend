66639b87a9558b10a1d87375905a7c9398f00189
ouzman376@gmail.com <ouzman376@gmail.com>
2026-02-08 12:35:33 +0100

    add categorie to Boutique

--- maps/serializers.py
+++ maps/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from rest_framework import serializers
 from .models import Hopital, Ecole, Parcelle, Commerce, Boutique
 
 
@@ -29,8 +30,18 @@ class CommerceSerializer(GeoFeatureModelSerializer):
         geo_field = "geom"
         fields = ("id", "nom", "adresse")
 
+
 class BoutiqueSerializer(GeoFeatureModelSerializer):
+    # "localisation" est un alias de "adresse"
+    localisation = serializers.CharField(source="adresse", allow_null=True, required=False)
+
+    # "catégorie" n'existe pas en base -> on la calcule (ou valeur fixe)
+    catégorie = serializers.SerializerMethodField()
+
     class Meta:
         model = Boutique
         geo_field = "geom"
         fields = ("id", "localisation", "catégorie")
+
+    def get_catégorie(self, obj):
+        return "Boutique"
