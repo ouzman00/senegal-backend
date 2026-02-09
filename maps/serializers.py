8963abe7716c3019ca0a1075b8167adf71d2e62d
ouzman376@gmail.com <ouzman376@gmail.com>
2026-02-09 17:12:10 +0100

    changement param

--- maps/serializers.py
+++ maps/serializers.py
@@ -50,4 +50,5 @@ class PointSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = Point
         geo_field = "geom"
-        fields = "__all__"
+        exclude = ("geom",)
+        # fields = ("id", "nom")
