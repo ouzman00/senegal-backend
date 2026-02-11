f30d7ac8eedd1a569afd9e5e3998abd2ba506501
ouzman376@gmail.com <ouzman376@gmail.com>
2026-02-11 10:23:53 +0100

    données changées

--- maps/admin.py
+++ maps/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Hopital, Ecole, Parcelle, Commerce, Boutique
+from .models import Hopital, Ecole, Parcelle, Commerce
 
 admin.site.register(Hopital)
 admin.site.register(Ecole)
 admin.site.register(Parcelle)
 admin.site.register(Commerce)
-admin.site.register(Boutique)
